89333a1bb8efdeaaf06462f3dcd03a3eeddc5394
ByteBlast <byteblast@gmail.com>
2015-02-22 09:51:06 +0000

    Seperated web model and web UI

---
+++ London.WebModel/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace London.WebModel
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello, World.");
+        }
+    }
+}
--- London/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="London.MvcApplication" Language="C#" %>
