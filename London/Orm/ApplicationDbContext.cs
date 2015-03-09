c9dec77126f45b988951f41d6fe7f4c5831a9b63
ByteBlast <byteblast@gmail.com>
2015-03-09 20:02:45 +0000

    Removed useless Identity code

---
+++ London/Orm/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using London.Orm.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace London.Orm
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- London/Startup.cs
+++ London/Startup.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using London.Identity;
+using London.Models;
+using London.Orm;
+using London.Orm.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Owin.Security.Providers.GitHub;
 
 [assembly: OwinStartupAttribute(typeof(London.Startup))]
 namespace London
@@ -8,7 +17,28 @@ namespace London
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseGitHubAuthentication(new GitHubAuthenticationOptions
+            {
+                ClientId = "dc2119b9f611c6fec9c6",
+                ClientSecret = "d028ee0e40ed333e53efa5dd2aba3e0e56bbfbee"
+            });
         }
     }
 }
